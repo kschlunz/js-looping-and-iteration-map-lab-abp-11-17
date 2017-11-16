10add0f927e8038b448988442e6a31121d9c7228
Kate Schlunz <schlunzk@gmail.com>
2017-11-16 19:01:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function lowerCaseDrivers(array){
     return e.toLowerCase()
   })
 }
+
+function nameToAttributes(names){
+  names.map(function(name){
+    let fName=name.split(" ")[0]
+    let lName=name.split(" ")[1]
+  })
+}
